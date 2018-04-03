92c40751286c79fa3e6170fd6d225e953a2fdb34
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-04-03 15:01:33 -0500

    Initial representative component

---
+++ govtrack.js
@@ -0,0 +1,5 @@
+const articleRef= document.querySelector(".missionStatement").innerHTML ="<i>Mission Statement: Just Legislate It</i>";
+
+const jimCooper = document.querySelector("#rep");
+jimCooper.setAttribute("id", "#congressional-district");
+
